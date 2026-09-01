4e4c389e554fc2fd122ec75378327ab5a64bdff6
rvd156 <90145605+rvd156@users.noreply.github.com>
2026-09-01 08:45:41 +0100

    Return Match events to normal

--- app/live/page.js
+++ app/live/page.js
@@ -841,31 +841,7 @@ const sohWon =
   event.event_type === 'manual_update'
 )
         .map(event => (
-         <div
-  key={event.id}
-  style={{
-  ...styles.scorerRow,
-  width: event.event_type === 'manual_update' ? '100%' : '82%',
-  marginLeft:
-    event.event_type === 'manual_update'
-      ? '0'
-      : event.team_id === homeTeam?.id
-        ? '0'
-        : 'auto',
-  marginRight:
-    event.event_type === 'manual_update'
-      ? '0'
-      : event.team_id === homeTeam?.id
-        ? 'auto'
-        : '0',
-  textAlign:
-    event.event_type === 'manual_update'
-      ? 'center'
-      : event.team_id === homeTeam?.id
-        ? 'left'
-        : 'right'
-}}
->
+        <div key={event.id} style={styles.scorerRow}>
   {event.match_minute}'{' '}
 
   {event.event_type === 'manual_update' ? (

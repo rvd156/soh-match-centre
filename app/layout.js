7d49c85b1a91f3b689595fa97b262767171c9670
rvd156 <90145605+rvd156@users.noreply.github.com>
2026-09-02 23:26:11 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import ServiceWorkerRegistration from './ServiceWorkerRegistration'
 
 export const metadata = {
   title: 'SOH Match Centre',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ServiceWorkerRegistration />
+        {children}
+      </body>
     </html>
   )
 }

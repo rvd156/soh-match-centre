aa0419c47e8b7e93a143850257fa2596add34678
rvd156 <90145605+rvd156@users.noreply.github.com>
2026-09-03 23:17:44 +0100

    Control Panel Update

--- app/page.js
+++ app/page.js
@@ -1360,10 +1360,6 @@ console.log('RESET RESULT:', data, error)
 
 <button
   onClick={() => {
-    if (!matchId) {
-      alert('No active match. Start or resume a match before adding a match update.')
-      return
-    }
 
     setManualUpdateText('')
     setManualUpdateOpen(true)

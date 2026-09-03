49391f03238cad7803acf3efc4a83ce15549511a
rvd156 <90145605+rvd156@users.noreply.github.com>
2026-09-03 19:21:34 +0100

    Create manifest.js

---
+++ app/manifest.js
@@ -0,0 +1,13 @@
+export default function manifest() {
+  return {
+    id: '/live',
+    name: 'SOH Match Centre',
+    short_name: 'SOH Match Centre',
+    description: 'SOH GAA live match scoreboard',
+    start_url: '/live',
+    scope: '/',
+    display: 'standalone',
+    background_color: '#071a12',
+    theme_color: '#071a12'
+  }
+}
